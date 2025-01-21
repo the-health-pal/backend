6c0280985f79eb2e4e4e3ce4e7b2ec821ae05bad
Rakindu Marambe <116582517+RakinduM@users.noreply.github.com>
2025-01-21 19:19:48 +0530

    Add OnModelCreating override in AppDbContext for model configuration

--- Config/AppDbContext.cs
+++ Config/AppDbContext.cs
@@ -8,4 +8,8 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
     public DbSet<UserModel> Users { get ; set;}
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
 }
